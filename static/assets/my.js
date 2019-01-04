2bcf5d07de30d2a39f9a7a602ad2a745f4b89a75
Kroes <Justin.Kroes@cdpr.ca.gov>
2019-01-04 15:00:01 -0800

    Move custom JS from footer.html to external file
    I had previously moved only one of two custom functions into an
    external file and maintained that single move as a commented out
    script element in case of failure. Given no issues with the first
    move, I moved the second function and removed the commented code
    from the first function.

--- static/assets/my.js
+++ static/assets/my.js
@@ -1,3 +1,4 @@
+/* Mimic behavior of position: sticky */
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
@@ -10,3 +11,24 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+/*
+https://vincenttam.gitlab.io/post/2018-09-17-fix-hugo-table-of-contents/
+Note that the output index.html files will have the same extraneous top-level
+ul and li elements, but differ in terms of whether the script code is included, 
+when alternately commenting and uncommenting the script below. Open the
+files in a browser to see that extraneous bullets/indentation have been 
+stripped by the script when it's included.
+*/
+(function() {
+  var toc = document.getElementById('TableOfContents');
+  if (!toc) return;
+  do {
+    var li, ul = toc.querySelector('ul');
+    if (ul.childElementCount !== 1) break;
+    li = ul.firstElementChild;
+    if (li.tagName !== 'LI') break;
+    // remove <ul><li></li></ul> where only <ul> only contains one <li>
+    ul.outerHTML = li.innerHTML;
+  } while (toc.childElementCount >= 1);
+})();
